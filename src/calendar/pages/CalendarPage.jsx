2f973cf7c85bf893aa0f84b8c33f4887df209029
Agustin Carthery <acarthery@Agustins-MacBook-Pro-2.local>
2022-11-24 00:36:15 -0300

    RTK logic and components

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,25 +1,16 @@
 import { Calendar } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-import { addHours } from 'date-fns';
 
-import { Navbar } from '../';
+import { Navbar, CalendarEvent, CalendarModal, FabAddNew, FabDelete } from '../';
 import { localizer, getMessagesEs } from '../../helpers';
-
-const events = [
-	{
-		title: 'Cumpleaños',
-		notes: 'Comprar regalo',
-		start: new Date(),
-		end: addHours(new Date(), 2),
-		bgColor: '#fafafa',
-		user: {
-			_id: '123',
-			name: 'Agustin',
-		},
-	},
-];
+import { useState } from 'react';
+import { useUiStore, useCalendarStore } from '../../hooks';
 
 export const CalendarPage = () => {
+	const { openDateModal } = useUiStore();
+	const { events, setActiveEvent } = useCalendarStore();
+	const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'week');
+
 	const eventStyleGetter = (event, start, end, isSelected) => {
 		const style = {
 			backgroundColor: '#347cf7',
@@ -31,6 +22,19 @@ export const CalendarPage = () => {
 			style,
 		};
 	};
+
+	const onDoubleClick = (event) => {
+		openDateModal();
+	};
+
+	const onSelect = (event) => {
+		console.log('selected');
+		setActiveEvent(event);
+	};
+
+	const onViewChanged = (event) => {
+		localStorage.setItem('lastView', event);
+	};
 	return (
 		<>
 			<Navbar />
@@ -39,12 +43,22 @@ export const CalendarPage = () => {
 				culture='es'
 				localizer={localizer}
 				events={events}
+				defaultView={lastView}
 				startAccessor='start'
 				endAccessor='end'
 				style={{ height: 'calc(100vh - 80px)' }}
 				messages={getMessagesEs()}
 				eventPropGetter={eventStyleGetter}
+				components={{
+					event: CalendarEvent,
+				}}
+				onDoubleClickEvent={onDoubleClick}
+				onSelectEvent={onSelect}
+				onView={onViewChanged}
 			/>
+			<CalendarModal />
+			<FabAddNew />
+			<FabDelete />
 		</>
 	);
 };
