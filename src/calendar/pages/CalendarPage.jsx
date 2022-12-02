f9b920070b1dedda3fe8a35f5eec934113df0812
Agustin Carthery <acarthery@Agustins-MacBook-Pro-2.local>
2022-12-01 22:36:33 -0300

    BE connection using custom hooks

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -3,17 +3,20 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 import { Navbar, CalendarEvent, CalendarModal, FabAddNew, FabDelete } from '../';
 import { localizer, getMessagesEs } from '../../helpers';
-import { useState } from 'react';
-import { useUiStore, useCalendarStore } from '../../hooks';
+import { useEffect, useState } from 'react';
+import { useUiStore, useCalendarStore, useAuthStore } from '../../hooks';
 
 export const CalendarPage = () => {
 	const { openDateModal } = useUiStore();
-	const { events, setActiveEvent } = useCalendarStore();
+	const { events, setActiveEvent, startLoadingEvents } = useCalendarStore();
+	const { user } = useAuthStore();
 	const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'week');
 
 	const eventStyleGetter = (event, start, end, isSelected) => {
+		const isMyEvent = user.uid === event.user._id || user.uid === event.user.uid;
+
 		const style = {
-			backgroundColor: '#347cf7',
+			backgroundColor: isMyEvent ? '#347cf7' : '#465660',
 			borderRadius: '0px',
 			opacity: 0.8,
 			color: 'white',
@@ -28,13 +31,17 @@ export const CalendarPage = () => {
 	};
 
 	const onSelect = (event) => {
-		console.log('selected');
 		setActiveEvent(event);
 	};
 
 	const onViewChanged = (event) => {
 		localStorage.setItem('lastView', event);
 	};
+
+	useEffect(() => {
+		startLoadingEvents();
+	}, []);
+
 	return (
 		<>
 			<Navbar />
--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -1,25 +1,56 @@
 import { useDispatch, useSelector } from "react-redux"
-import { onSetActiveEvent, onAddNewEvent, onUpdateEvent, onDeleteEvent } from "../store/calendar/calendarSlice"
+import Swal from "sweetalert2"
+import { calendarApi } from '../api'
+import { convertEventsToDateEvents } from "../helpers"
+import { onSetActiveEvent, onAddNewEvent, onUpdateEvent, onDeleteEvent, onLoadEvents } from "../store/calendar/calendarSlice"
 
 export const useCalendarStore = () => {
   const dispatch = useDispatch()
   const { events, activeEvent } = useSelector(state => state.calendar)
+  const { user } = useSelector(state => state.auth)
 
   const setActiveEvent = (calendarEvent) => {
     dispatch(onSetActiveEvent(calendarEvent))
   }
 
   const startSavingEvent = async (calendarEvent) => {
-    if (calendarEvent._id) {
-      dispatch(onUpdateEvent(calendarEvent))
-    }
-    else {
-      dispatch(onAddNewEvent({ ...calendarEvent, _id: new Date().getTime() }))
+
+    try {
+      if (calendarEvent.id) {
+        await calendarApi.put(`/events/${calendarEvent.id}`, calendarEvent)
+
+        dispatch(onUpdateEvent({ ...calendarEvent, user }))
+        return
+      }
+
+      const { data } = await calendarApi.post('/events', calendarEvent)
+      dispatch(onAddNewEvent({ ...calendarEvent, id: data.event.id, user }))
+    } catch (error) {
+      console.log(error);
+      Swal.fire('Error al guardar', error.response.data.msg, 'error')
     }
   }
 
-  const startDeletingEvent = () => {
-    dispatch(onDeleteEvent())
+  const startDeletingEvent = async () => {
+    try {
+      await calendarApi.delete(`/events/${activeEvent.id}`)
+      dispatch(onDeleteEvent())
+    } catch (error) {
+      console.log(error);
+      Swal.fire('Error al eliminar', error.response.data.msg, 'error')
+    }
+  }
+
+  const startLoadingEvents = async () => {
+    try {
+      const { data } = await calendarApi.get('/events')
+      const events = convertEventsToDateEvents(data.events)
+      dispatch(onLoadEvents(events))
+
+    } catch (error) {
+      console.log('Error en carga');
+      console.log(error);
+    }
   }
 
   return {
@@ -28,6 +59,7 @@ export const useCalendarStore = () => {
     hasEventSelected: !!activeEvent,
     setActiveEvent,
     startSavingEvent,
-    startDeletingEvent
+    startDeletingEvent,
+    startLoadingEvents
   }
 }
